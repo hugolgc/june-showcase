501f8c56f4ba3bb1926c45c07adb62da25e553ae
hugolgc <lagache.hugo@hotmail.fr>
2024-10-17 10:02:08 +0200

    Start home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 
 export default function Index() {
   return <>
@@ -9,6 +8,6 @@ export default function Index() {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.svg" />
     </Head>
-    <Image src="/showcase.jpg" alt="Page d'accueil" width={1440} height={3872} />
+    <img src="/showcase.jpg" alt="Page d'accueil" />
   </>
 }
